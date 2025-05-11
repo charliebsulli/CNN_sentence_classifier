259acb45e33f66fb3ca493feb9bb4c183fa45694
Charles <charlessullivan@brandeis.edu>
2025-05-11 13:35:28 -0700

    update utils to use trec dataset for vocab

--- utils.py
+++ utils.py
@@ -5,10 +5,10 @@ from datasets import load_dataset
 from cnn import UNK_TOKEN, PADDING_TOKEN
 
 
-def get_vocab(dataset):
+def get_vocab(dataset, text_field, delim=" "):
     vocab = set()
     for sample in dataset:
-        vocab.update(sample["sentence"].split(" "))
+        vocab.update(sample[text_field].split(delim))
     vocab_dict = {tok: i+2 for i, tok in enumerate(vocab)}
     vocab_dict[PADDING_TOKEN] = 0
     vocab_dict[UNK_TOKEN] = 1
@@ -27,9 +27,12 @@ def get_embeddings(vocab, model, embeddings_size):
 
 
 if __name__ == "__main__":
-    train = load_dataset("stanfordnlp/sst2", split="train")
+    train = load_dataset("CogComp/trec", split="train")
+    ds = train.train_test_split(test_size=0.1, shuffle=True, seed=2)
+    train = ds["train"]
+    dev = ds["test"]
     word2vec = gensim.downloader.load("word2vec-google-news-300")
-    vocab = get_vocab(train)
+    vocab = get_vocab(train, "text")
     embeddings = get_embeddings(vocab, word2vec, 300)
     embeddings_dict = {"vocab": vocab, "embeddings": embeddings}
     torch.save(embeddings_dict, "embeddings.pt")
